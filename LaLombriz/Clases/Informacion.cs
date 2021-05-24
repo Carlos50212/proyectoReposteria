86b7a432b467b3c72769f9d9dc2455b67e327044
Carlos RL <carlosruiz50212@gmail.com>
2021-05-23 20:16:38 -0500

    *CHANGELOG* -Se agrego el controlador de la API de pago -Se agrego clase información para recibir los datos del pago -Versión 2.0.2021.05.23 */CHANGELOG*

---
+++ LaLombriz/Clases/Informacion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LaLombriz.Clases
+{
+    public class Informacion
+    {
+        [Required]
+        [MaxLength(140)]
+        public string transactionAmount { get; set; }
+        [Required]
+        [MaxLength(140)]
+        public string correo { get; set; }
+        [Required]
+        [MaxLength(140)]
+        public string description { get; set; }
+        [Required]
+        [MaxLength(140)]
+        public string token { get; set; }
+        [Required]
+        [MaxLength(140)]
+        public string issuer_id { get; set; }
+        [Required]
+        [MaxLength(140)]
+        public string installments { get; set; }
+        [Required]
+        [MaxLength(140)]
+        public string payment_method_id { get; set; }
+    }
+}
--- LaLombriz/Global.asax.cs
+++ LaLombriz/Global.asax.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using LaLombriz.App_Start;
 
 namespace LaLombriz
 {
@@ -16,6 +18,7 @@ namespace LaLombriz
             // Código que se ejecuta al iniciar la aplicación
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
